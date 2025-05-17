279206f789f9ba894b3c8f96d63fea3b437158cc
Phuong Van <vnphuong16@outlook.com>
2025-05-17 19:27:50 +0700

    Update .gitignore, create CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// @author: PaNda
+// @time: 2025-05-17 19:19:58
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <iomanip>
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	// Code here
+	std::cout << "Hello World\n";
+
+	return 0;
+}
